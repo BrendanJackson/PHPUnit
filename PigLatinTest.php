a99a3dc5c8305f08a548d0a9742d988ea53fab5c
BrendanJackson <bajackson859@gmail.com>
2016-08-19 11:21:36 -0400

    PHPUnit Test initial commit

---
+++ PigLatinTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chris Gerhardstein
+ * Date: 8/18/2016
+ * Time: 4:07 PM
+ */
+class PigLatinTest extends PHPUnit_Framework_TestCase
+{
+    public function englishToPigLatinWorkCorrectly()
+    {
+        /**
+         * Given I have an english word
+         * If I pass that word to my PigLAtin converter
+         * I get back the correctly transformed version
+         */
+        $this->assertTrue(false);
+    }
+}
